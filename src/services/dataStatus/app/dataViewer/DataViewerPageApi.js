d1168314339829d8132dfa21f67479d694abe4c7
우정은 <dnwjddms13@gmail.com>
2026-02-03 21:40:56 +0900

    [feature] 로딩바 추가

--- src/services/dataStatus/app/dataViewer/DataViewerPageApi.js
+++ src/services/dataStatus/app/dataViewer/DataViewerPageApi.js
@@ -1,19 +1,26 @@
-import { useMutation, useQuery } from "react-query";
+import { useMutation } from "react-query";
 import api from "@/common/queries/Api.js";
+import { useContext } from "react";
+import { loadingSpinnerContext } from "@/components/common/LoadingSpinner.jsx";
+
 
 export function DataViewerPageApi() {
-    /** 문항 목록 조회 (예시) */
-    const getVariableList = useMutation(
-        async () => await api.post({}, "/pages/be30866e-b079-44b3-a26c-ad75f89c5134/variables/recoded", "API_BASE_URL_DATASTATUS")
-    );
+    const loadingSpinner = useContext(loadingSpinnerContext);
 
     /** 전체 데이터 행 조회 */
     const getPageRows = useMutation(
-        async (data) => await api.post(data, "/pages/rows", "API_BASE_URL_DATASTATUS")
+        async (data) => await api.post(data, "/pages/rows", "API_BASE_URL_DATASTATUS"),
+        {
+            onMutate: (vars) => {
+                loadingSpinner.show();
+            },
+            onSettled: () => {
+                loadingSpinner.hide();
+            }
+        }
     );
 
     return {
-        getVariableList,
         getPageRows
     };
 }
--- src/services/dataStatus/app/variable/VariablePageApi.js
+++ src/services/dataStatus/app/variable/VariablePageApi.js
@@ -1,12 +1,23 @@
-import { useMutation, useQuery } from "react-query";
+import { useMutation } from "react-query";
 import api from "@/common/queries/Api.js";
+import { useContext } from "react";
+import { loadingSpinnerContext } from "@/components/common/LoadingSpinner.jsx";
 
 export function VariablePageApi() {
 
+    const loadingSpinner = useContext(loadingSpinnerContext);
 
     /** 원본 변수 목록 조회 */
     const getOriginalVariables = useMutation(
-        async (data) => await api.post(data, "/pages/variables", "API_BASE_URL_DATASTATUS")
+        async (data) => await api.post(data, "/pages/variables", "API_BASE_URL_DATASTATUS"),
+        {
+            onMutate: (vars) => {
+                loadingSpinner.show();
+            },
+            onSettled: () => {
+                loadingSpinner.hide();
+            }
+        }
     );
 
     return {
