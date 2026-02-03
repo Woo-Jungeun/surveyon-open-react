f7e0ee0582d23f501a66676452efd3a4822b1082
우정은 <dnwjddms13@gmail.com>
2026-02-03 14:03:06 +0900

    [fix] api url 수정

--- src/services/dataStatus/app/variable/VariablePageApi.js
+++ src/services/dataStatus/app/variable/VariablePageApi.js
@@ -4,7 +4,7 @@ import api from "@/common/queries/Api.js";
 export function VariablePageApi() {
     /** 문항 목록 조회 */
     const getVariableList = useMutation(
-        async (data) => await api.post(data, "/page", "API_BASE_URL_DATASTATUS")
+        async (data) => await api.post(data, "/pages", "API_BASE_URL_DATASTATUS")
     );
 
     const getVariableList1 = useMutation(
