b901af80a6032e848551a56a0b37def47f858482
우정은 <dnwjddms13@gmail.com>
2026-03-19 10:33:07 +0900

    [fix] localhost에서  hostname이  EX_API_BASE_URL일때 "https://son.hrc.kr/o", 이걸로

--- public/config.js
+++ public/config.js
@@ -1,13 +1,14 @@
 (function () {
     // 접속한 브라우저의 도메인(IP 등 호스트네임) 확인
     var hostname = window.location.hostname;
+    console.log(hostname)
     // 현재 위치가 테스트 서버(stest 포함)이거나 로컬 테스트 환경(localhost/127)인지 판별
     var isTestServer = hostname.includes('stest') || hostname === 'localhost' || hostname === '127.0.0.1';
 
     if (isTestServer) {
         // === [테스트 서버 자동 적용] ===
         window.API_CONFIG = {
-            EX_API_BASE_URL: "https://stest.hrc.kr/APIx",
+            EX_API_BASE_URL: (hostname === 'localhost' || hostname === '127.0.0.1') ? "https://son.hrc.kr/o" : "https://stest.hrc.kr/APIx",
             API_BASE_URL_OPENAI: "https://stest.hrc.kr/APIs/o",
             API_BASE_URL_BOARD: "https://stest.hrc.kr/APIs/n",
             API_BASE_URL_DATASTATUS: "https://stest.hrc.kr/APIs/s",
